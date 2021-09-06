33c82f207316a168aea7fbecaec552fe28db599b
muhammadikrama9@outlook.com <muhammadikrama9@outlook.com>
2021-09-06 04:41:24 -0700

    Add project files.

---
+++ Loops/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            for(int a = 0; a <=10; a++ )
+            {
+                Console.WriteLine(a);
+            }
+            Console.ReadKey();
+        }
+    }
+}
